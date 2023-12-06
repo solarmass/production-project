7aca93914e02985769225070043f58ac15a17b94
Георгий Иванов <georgijivanov@Georgijs-MacBook-Pro.local>
2023-12-06 20:24:22 +0500

    add navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,14 +3,14 @@ import "./styles/index.scss";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
 import { AppRouter } from "app/providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
 	const { theme, toggleTheme } = useTheme();
 	return (
 		<div className={classNames("app", {}, [theme])}>
+			<Navbar />
 			<button onClick={toggleTheme}>TOGGLE</button>
-			<Link to={"/"}>Главная</Link>
-			<Link to={"/about"}>О приложении</Link>
 			<AppRouter />
 		</div>
 	);
