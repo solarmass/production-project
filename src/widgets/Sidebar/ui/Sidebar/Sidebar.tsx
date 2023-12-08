cf343eb2838389df77bef85986a263688bdfef46
Георгий Иванов <georgijivanov@Georgijs-MacBook-Pro.local>
2023-12-08 21:22:36 +0500

    add i18n

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
+import { LangSwitcher } from "widgets/LangSwitcher";
 import { ThemeSwitcher } from "widgets/ThemeSwitcher";
 import cls from "./Sidebar.module.scss";
 
@@ -24,6 +25,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
 			<button onClick={onToggle}>toggle</button>
 			<div className={cls.switchers}>
 				<ThemeSwitcher />
+				<LangSwitcher className={cls.lang} />
 			</div>
 		</div>
 	);
