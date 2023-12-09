75346dd2a34700a183396b4c05c56f72ac5b4ec8
Георгий Иванов <georgijivanov@Georgijs-MacBook-Pro.local>
2023-12-09 15:27:21 +0500

    add babel loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -8,6 +8,17 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
 		use: ["@svgr/webpack"],
 	};
 
+	const babelLoader = {
+		test: /\.(js|jsx|tsx)$/,
+		exclude: /node_modules/,
+		use: {
+			loader: "babel-loader",
+			options: {
+				presets: ["@babel/preset-env"],
+			},
+		},
+	};
+
 	const fileLoader = {
 		test: /\.(png|jpe?g|gif|woff2|woff)$/i,
 		use: [
@@ -46,5 +57,5 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
 		exclude: /node_modules/,
 	};
 
-	return [typescriptLoader, cssLoader, svgrLoader, fileLoader];
+	return [cssLoader, babelLoader, typescriptLoader, svgrLoader, fileLoader];
 }
