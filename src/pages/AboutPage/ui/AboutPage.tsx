72367c6dbe1c02e43c1f391748b10760b7e702c8
Георгий Иванов <georgijivanov@Georgijs-MacBook-Pro.local>
2023-12-26 19:00:23 +0500

    add redux store, add first entity

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,9 +1,15 @@
 import { useTranslation } from 'react-i18next';
+import { Counter } from 'entities/Counter';
 
 const AboutPage = () => {
     const { t } = useTranslation('about');
 
-    return <div>{t('О сайте')}</div>;
+    return (
+        <div>
+            {t('О сайте')}
+            <Counter />
+        </div>
+    );
 };
 
 export default AboutPage;
