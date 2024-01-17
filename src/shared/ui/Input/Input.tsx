95ee449176137bca34a51deb44311fa5b337e70d
Георгий Иванов <georgijivanov@Georgijs-MacBook-Pro.local>
2024-01-17 21:03:19 +0500

    add new input, add login form ui

---
+++ src/shared/ui/Input/Input.tsx
@@ -0,0 +1,58 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import React, {
+    InputHTMLAttributes, memo, useEffect, useRef,
+} from 'react';
+import cls from './Input.module.scss';
+
+type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>
+
+interface InputProps extends HTMLInputProps {
+    className?: string;
+    value?: string;
+    autofocus?: boolean;
+    onChange?: (value: string) => void;
+}
+
+export const Input = memo((props: InputProps) => {
+    const {
+        className,
+        value,
+        onChange,
+        type = 'text',
+        placeholder,
+        autofocus,
+        ...otherProps
+    } = props;
+
+    const ref = useRef<HTMLInputElement>();
+
+    useEffect(() => {
+        if (autofocus) {
+            ref.current.focus();
+        }
+    }, [autofocus]);
+
+    const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        onChange?.(e.target.value);
+    };
+
+    return (
+        <div className={classNames(cls.InputWrapper, {}, [className])}>
+            {placeholder && (
+                <div className={cls.placeholder}>
+                    {`${placeholder}>`}
+                </div>
+            )}
+            <div>
+                <input
+                    ref={ref}
+                    className={cls.input}
+                    type={type}
+                    value={value}
+                    onChange={onChangeHandler}
+                    {...otherProps}
+                />
+            </div>
+        </div>
+    );
+});
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,9 +1,8 @@
 import React, { useCallback, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
-import { Modal } from 'shared/ui/Modal/Modal';
+import { LoginModal } from 'features/AuthByUserName';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -14,8 +13,12 @@ export function Navbar({ className }: NavbarProps) {
     const { t } = useTranslation();
     const [isAuthModal, setIsAuthModal] = useState(false);
 
-    const onToggleModal = useCallback(() => {
-        setIsAuthModal((prev) => !prev);
+    const onCloseModal = useCallback(() => {
+        setIsAuthModal(false);
+    }, []);
+
+    const onShowModal = useCallback(() => {
+        setIsAuthModal(true);
     }, []);
 
     return (
@@ -23,16 +26,13 @@ export function Navbar({ className }: NavbarProps) {
             <Button
                 theme={ButtonTheme.CLEAR_INVERTED}
                 className={cls.links}
-                onClick={onToggleModal}
+                onClick={onShowModal}
             >
                 {t('Войти')}
 
             </Button>
 
-            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
-                {/* eslint-disable-next-line */}
-                {t('Est commodo deserunt et nisi ad. Id labore ullamco laborum occaecat tempor et ipsum id. Commodo dolore ipsum qui fugiat aliquip. Velit amet ex cupidatat aliqua laborum. Consequat excepteur dolor ullamco in quis magna quis elit incididunt ex. Non ex non enim dolor consectetur ut id. Minim occaecat Lorem adipisicing nostrud mollit eu.')}
-            </Modal>
+            <LoginModal isOpen={isAuthModal} onClose={onCloseModal} />
         </div>
     );
 }
