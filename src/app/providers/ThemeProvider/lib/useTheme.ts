76cc99e6ea4fdbeca80ce2eba37129dc700a8210
Георгий Иванов <georgijivanov@Georgijs-MacBook-Pro.local>
2024-02-09 20:59:13 +0500

    add new theme

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -16,7 +16,21 @@ export const useTheme: () => UseThemeResult = () => {
     return {
         theme: theme || Theme.LIGHT,
         toggleTheme: () => {
-            const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+            let newTheme:Theme;
+
+            switch (theme) {
+            case Theme.DARK:
+                newTheme = Theme.LIGHT;
+                break;
+            case Theme.LIGHT:
+                newTheme = Theme.ORANGE;
+                break;
+            case Theme.ORANGE:
+                newTheme = Theme.DARK;
+                break;
+            default:
+                newTheme = Theme.LIGHT;
+            }
             setTheme?.(newTheme);
             document.body.className = newTheme;
             localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
