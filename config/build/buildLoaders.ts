e60fb8228cb62d86c0f184326352fde7d4546ecc
Георгий Иванов <georgijivanov@Georgijs-MacBook-Pro.local>
2024-04-28 00:35:40 +0500

    configure babel for typescript, add fork ts checker, add custom babel plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -9,7 +9,8 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
         use: ['@svgr/webpack'],
     };
 
-    const babelLoader = buildBabelLoader(options);
+    const codeBabelLoader = buildBabelLoader({ ...options, isTsx: false });
+    const tsxCodeBabelLoader = buildBabelLoader({ ...options, isTsx: true });
 
     const fileLoader = {
         test: /\.(png|jpe?g|gif|woff2|woff)$/i,
@@ -23,11 +24,11 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     const cssLoader = buildCssLoader(options.isDev);
 
     // Если не используем тайпскрипт - нужен babel-loader
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    };
+    // const typescriptLoader = {
+    //     test: /\.tsx?$/,
+    //     use: 'ts-loader',
+    //     exclude: /node_modules/,
+    // };
 
-    return [cssLoader, babelLoader, typescriptLoader, svgrLoader, fileLoader];
+    return [cssLoader, codeBabelLoader, tsxCodeBabelLoader, /* typescriptLoader, */ svgrLoader, fileLoader];
 }
--- config/build/loaders/buildBabelLoader.ts
+++ config/build/loaders/buildBabelLoader.ts
@@ -1,13 +1,34 @@
+import babelRemovePropsPlugin from '../babel/babelRemovePropsPlugin';
 import { BuildOptions } from '../types/config';
 
-export function buildBabelLoader(options: BuildOptions) {
+interface BuildBabelLoaderProps extends BuildOptions {
+   isTsx: boolean;
+}
+
+export function buildBabelLoader({ isTsx, isDev }: BuildBabelLoaderProps) {
     return {
-        test: /\.(js|jsx|tsx)$/,
+        test: isTsx ? /\.(jsx|tsx)$/ : /\.(js|ts)$/,
         exclude: /node_modules/,
         use: {
             loader: 'babel-loader',
             options: {
                 presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        '@babel/plugin-transform-typescript',
+                        {
+                            isTsx,
+                        },
+                    ],
+                    '@babel/plugin-transform-runtime',
+                    isTsx && [
+                        babelRemovePropsPlugin,
+                        {
+                            props: ['data-testid'],
+                        },
+                    ],
+                    isDev && require.resolve('react-refresh/babel'),
+                ].filter(Boolean),
             },
         },
     };
