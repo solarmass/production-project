a3aceb9f1ea00e21a96024ca7a7f867e772915ae
Георгий Иванов <georgijivanov@Georgijs-MacBook-Pro.local>
2024-04-28 20:39:36 +0500

    add star rating, add Rating entity and rating card

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,5 +1,7 @@
-import { ButtonHTMLAttributes, FC, memo } from 'react';
-import { Mods, classNames } from '@/shared/lib/classNames/classNames';
+import {
+    ButtonHTMLAttributes, FC, memo, ReactNode,
+} from 'react';
+import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import cls from './Button.module.scss';
 
 export enum ButtonTheme {
@@ -8,31 +10,34 @@ export enum ButtonTheme {
     OUTLINE = 'outline',
     OUTLINE_RED = 'outline_red',
     BACKGROUND = 'background',
-    BACKGROUND_INVERTED = 'backgroundInverted'
+    BACKGROUND_INVERTED = 'backgroundInverted',
 }
 
 export enum ButtonSize {
     M = 'size_m',
     L = 'size_l',
-    XL = 'size_xl'
+    XL = 'size_xl',
 }
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
     className?: string;
     theme?: ButtonTheme;
     square?: boolean;
     size?: ButtonSize;
     disabled?: boolean;
+    children?: ReactNode;
+    fullWidth?: boolean;
 }
 
-export const Button: FC<ButtonProps> = memo((props: ButtonProps) => {
+export const Button = memo((props: ButtonProps) => {
     const {
         className,
         children,
         theme = ButtonTheme.OUTLINE,
         square,
-        size = ButtonSize.M,
         disabled,
+        fullWidth,
+        size = ButtonSize.M,
         ...otherProps
     } = props;
 
@@ -41,6 +46,7 @@ export const Button: FC<ButtonProps> = memo((props: ButtonProps) => {
         [cls.square]: square,
         [cls[size]]: true,
         [cls.disabled]: disabled,
+        [cls.fullWidth]: fullWidth,
     };
 
     return (
